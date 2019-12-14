67c725bdd27f81368aefe321128c0d655cbfd81a
Arvind Dangar <arvindkumar.d@advanced-india.com>
2019-12-14 15:15:59 +0530

    initial code

---
+++ server/app/routes/user.routes.js
@@ -0,0 +1,20 @@
+module.exports = (app) => {
+    const users = require('../controllers/user.controller');
+
+    // Create a new User
+    app.post('/users', users.create);
+
+    app.post('/users/login', users.login);
+
+    // Retrieve all User
+    app.get('/users', users.findAll);
+
+    // Retrieve a single user with userId
+    app.get('/users/:userId', users.findOne);
+
+    // Update a User with userId
+    app.put('/users/:userId', users.update);
+
+    // Delete a User with userId
+    app.delete('/users/:userId', users.delete);
+}
---
+++ server/config/database-config.js
@@ -0,0 +1,5 @@
+module.exports = {
+   // url: 'mongodb+srv://dbuser:dbuser1@cluster0-o53bs.mongodb.net/test?retryWrites=true&w=majority'
+    url: 'mongodb://mongo/test' // works on docker only 
+   // url: 'mongodb://localhost/test' //works with npm run start
+}
